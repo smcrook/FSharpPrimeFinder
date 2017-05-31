50b063855cffe617cb38f7af6482e1d4d5404904
Scott Crook <scott.crook@macom.com>
2017-05-31 09:32:08 -0400

    Using Sieve of Erastosthnes from https://stackoverflow.com/questions/1097411/learning-f-printing-prime-numbers

---
+++ PrimeFinder/PrimeFinder/Program.fs
@@ -0,0 +1,20 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+
+let testList = [2..100]
+
+
+let rec sieve = function
+    | (p::xs) -> p :: sieve [ for x in xs do if x % p > 0 then yield x ]
+    | []      -> []
+
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    let primes = sieve testList
+    printfn "%A" primes 
+
+    0 // return an integer exit code
+
